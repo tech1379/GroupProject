5865977d8f8722ad0008b4f8ebbe441b301cfb4d
Michal Downing-Wyatt <mrdowning@mymail.tstc.edu>
2021-11-05 11:38:01 -0500

    changed back Run to formSplash

--- Team3/Program.cs
+++ Team3/Program.cs
@@ -16,7 +16,7 @@ namespace Team3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmClockInClockOut());
+            Application.Run(new frmSplash());
         }
     }
 }
