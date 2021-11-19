f9873048aa4a2d1915f77c1399e64eaa31f36c47
Michal Downing-Wyatt <mrdowning@mymail.tstc.edu>
2021-11-19 15:11:14 -0600

    Updated payroll

--- Team3/frmPayroll.Designer.cs
+++ Team3/frmPayroll.Designer.cs
@@ -30,10 +30,8 @@
         {
             this.lbxOutput = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblSocSecurityTax = new System.Windows.Forms.Label();
-            this.lblFICA = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.lblGross = new System.Windows.Forms.Label();
+            this.lblGrossPay = new System.Windows.Forms.Label();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.tbxRateOfPay = new System.Windows.Forms.TextBox();
@@ -53,17 +51,16 @@
             // lbxOutput
             // 
             this.lbxOutput.FormattingEnabled = true;
-            this.lbxOutput.Location = new System.Drawing.Point(390, 26);
+            this.lbxOutput.Location = new System.Drawing.Point(390, 44);
             this.lbxOutput.Name = "lbxOutput";
-            this.lbxOutput.Size = new System.Drawing.Size(389, 407);
+            this.lbxOutput.Size = new System.Drawing.Size(389, 316);
             this.lbxOutput.TabIndex = 8;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.lblSocSecurityTax);
-            this.groupBox1.Controls.Add(this.lblFICA);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.lblGross);
+            this.groupBox1.Controls.Add(this.lblGrossPay);
+            this.groupBox1.Controls.Add(this.btnClose);
             this.groupBox1.Controls.Add(this.btnClear);
             this.groupBox1.Controls.Add(this.btnCalculate);
             this.groupBox1.Controls.Add(this.tbxRateOfPay);
@@ -77,66 +74,50 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(21, 17);
+            this.groupBox1.Location = new System.Drawing.Point(21, 48);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(350, 357);
+            this.groupBox1.Size = new System.Drawing.Size(350, 309);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             // 
-            // lblSocSecurityTax
+            // lblGross
             // 
-            this.lblSocSecurityTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblSocSecurityTax.Location = new System.Drawing.Point(118, 306);
-            this.lblSocSecurityTax.Name = "lblSocSecurityTax";
-            this.lblSocSecurityTax.Size = new System.Drawing.Size(100, 25);
-            this.lblSocSecurityTax.TabIndex = 18;
+            this.lblGross.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGross.Location = new System.Drawing.Point(118, 262);
+            this.lblGross.Name = "lblGross";
+            this.lblGross.Size = new System.Drawing.Size(100, 25);
+            this.lblGross.TabIndex = 17;
             // 
-            // lblFICA
+            // lblGrossPay
             // 
-            this.lblFICA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblFICA.Location = new System.Drawing.Point(118, 262);
-            this.lblFICA.Name = "lblFICA";
-            this.lblFICA.Size = new System.Drawing.Size(100, 25);
-            this.lblFICA.TabIndex = 17;
-            // 
-            // label7
-            // 
-            this.label7.Location = new System.Drawing.Point(15, 303);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 23);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Social Sec. Tax";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label8
-            // 
-            this.label8.Location = new System.Drawing.Point(15, 259);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(100, 23);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "FICA";
-            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblGrossPay.Location = new System.Drawing.Point(15, 259);
+            this.lblGrossPay.Name = "lblGrossPay";
+            this.lblGrossPay.Size = new System.Drawing.Size(100, 23);
+            this.lblGrossPay.TabIndex = 12;
+            this.lblGrossPay.Text = "Gross Pay";
+            this.lblGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btnClear
             // 
             this.btnClear.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(240, 262);
+            this.btnClear.Location = new System.Drawing.Point(240, 167);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(97, 26);
             this.btnClear.TabIndex = 3;
             this.btnClear.Text = "C&lear";
             this.btnClear.UseVisualStyleBackColor = true;
-
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnCalculate
             // 
             this.btnCalculate.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalculate.Location = new System.Drawing.Point(240, 306);
+            this.btnCalculate.Location = new System.Drawing.Point(240, 214);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(97, 26);
             this.btnCalculate.TabIndex = 2;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // tbxRateOfPay
             // 
@@ -180,7 +161,7 @@
             this.label5.Size = new System.Drawing.Size(100, 23);
             this.label5.TabIndex = 4;
             this.label5.Text = "Hourly Rate";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label4
             // 
@@ -189,7 +170,7 @@
             this.label4.Size = new System.Drawing.Size(100, 23);
             this.label4.TabIndex = 3;
             this.label4.Text = "Hours Worked";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label3
             // 
@@ -198,7 +179,7 @@
             this.label3.Size = new System.Drawing.Size(114, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "Work Week End";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label2
             // 
@@ -207,7 +188,7 @@
             this.label2.Size = new System.Drawing.Size(114, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Work Week Start";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label1
             // 
@@ -216,29 +197,29 @@
             this.label1.Size = new System.Drawing.Size(100, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "EmployeeID";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(261, 393);
+            this.btnClose.Location = new System.Drawing.Point(240, 261);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(97, 40);
+            this.btnClose.Size = new System.Drawing.Size(97, 26);
             this.btnClose.TabIndex = 16;
             this.btnClose.Text = "Cl&ose";
             this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // frmPayroll
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Team3.Properties.Resources.Background;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 405);
             this.Controls.Add(this.lbxOutput);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btnClose);
             this.Name = "frmPayroll";
-            this.Text = "frmPayroll";
+            this.Text = "Payroll";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -248,11 +229,9 @@
         #endregion
         private System.Windows.Forms.ListBox lbxOutput;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label lblSocSecurityTax;
-        private System.Windows.Forms.Label lblFICA;
+        private System.Windows.Forms.Label lblGross;
         private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblGrossPay;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.TextBox tbxRateOfPay;
